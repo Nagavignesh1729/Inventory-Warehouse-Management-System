1f37f6bf0f8839198845ee84fda94581a6743f66
shru <shruthishdeepneer@gmail.com>
2025-10-14 22:03:31 +0530

    Removed duplicates

--- warehouse-backend/warehouse-backend/src/middlewares/validate.middleware.js
+++
@@ -1,9 +0,0 @@
-const { error } = require('../utils/response');
-
-module.exports = (schema) => (req, res, next) => {
-  const { error: validationError } = schema.validate(req.body);
-  if (validationError) {
-    return error(res, validationError.details[0].message, 400);
-  }
-  next();
-};
